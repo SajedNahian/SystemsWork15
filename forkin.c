da0f60d358a43002ac2893457ff37398057b82ff
snahian <contactsajednahian@gmail.com>
2018-11-18 17:37:46 -0500

    fixed indendation from emacs

--- forkin.c
+++ forkin.c
@@ -13,19 +13,19 @@ int main()
 		if (!processID) { 
 			// runs if not parent
 			int fd = open("/dev/random", O_RDONLY);
-           	int * randomSeconds = malloc(sizeof(int));
-            read(fd, randomSeconds, sizeof(int));
-            (*randomSeconds) = abs(((*randomSeconds)) % 16) + 5;
-            printf("Child with id: %d is sleeping for %d seconds. Be quiet!\n", getpid(), *randomSeconds);
-            sleep(*randomSeconds);
-            printf("Child with id: %d has woken up!\n", getpid());
+			int * randomSeconds = malloc(sizeof(int));
+			read(fd, randomSeconds, sizeof(int));
+			(*randomSeconds) = abs(((*randomSeconds)) % 16) + 5;
+			printf("Child with id: %d is sleeping for %d seconds. Be quiet!\n", getpid(), *randomSeconds);
+			sleep(*randomSeconds);
+			printf("Child with id: %d has woken up!\n", getpid());
             // allow parent to get how long the child slept for
-            exit(*randomSeconds);
+			exit(*randomSeconds);
 		}
 	}
 	int status = -1;
-    int processId = wait(&status);
-    printf("My favorite child has the id of %d because he only slept for %d seconds\n", processId, WEXITSTATUS(status));
-    printf("My other child will wake up soon but I (the parent) am done\n");
+	int processId = wait(&status);
+	printf("My favorite child has the id of %d because he only slept for %d seconds\n", processId, WEXITSTATUS(status));
+	printf("My other child will wake up soon but I (the parent) am done\n");
 	return 0;
 }
